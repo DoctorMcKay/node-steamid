1b5801f68e2723e9ef7926e359c7cc3f8fc45569
Alexander Corn <mckay@doctormckay.com>
2015-02-28 19:34:21 -0500

    Added tests

---
+++ test.js
@@ -0,0 +1,171 @@
+var SteamID = require('./index.js');
+var assert = require('assert');
+
+function start(test) {
+	process.stdout.write("testing " + test + "... ");
+}
+
+function end() {
+	console.log("ok");
+}
+
+function checkProperty(obj, prop, expected) {
+	assert.strictEqual(obj[prop], expected, "unexpected " + prop + " value " + obj[prop]);
+}
+
+function checkProperties(obj, expected) {
+	for(var i in expected) {
+		checkProperty(obj, i, expected[i]);
+	}
+}
+
+try {
+	var sid, val;
+	
+	start("parameterless construction");
+	sid = new SteamID();
+	checkProperties(sid, {
+		"universe": SteamID.Universe.INVALID,
+		"type": SteamID.Type.INVALID,
+		"instance": SteamID.Instance.ALL,
+		"accountid": 0
+	});
+	end();
+	
+	start("steam2id construction (universe 0)");
+	sid = new SteamID("STEAM_0:0:23071901");
+	checkProperties(sid, {
+		"universe": SteamID.Universe.PUBLIC,
+		"type": SteamID.Type.INDIVIDUAL,
+		"instance": SteamID.Instance.DESKTOP,
+		"accountid": 46143802
+	});
+	end();
+	
+	start("steam2id construction (universe 1)");
+	sid = new SteamID("STEAM_1:1:23071901");
+	checkProperties(sid, {
+		"universe": SteamID.Universe.PUBLIC,
+		"type": SteamID.Type.INDIVIDUAL,
+		"instance": SteamID.Instance.DESKTOP,
+		"accountid": 46143803
+	});
+	end();
+	
+	start("steam3id construction (individual)");
+	sid = new SteamID("[U:1:46143802]");
+	checkProperties(sid, {
+		"universe": SteamID.Universe.PUBLIC,
+		"type": SteamID.Type.INDIVIDUAL,
+		"instance": SteamID.Instance.DESKTOP,
+		"accountid": 46143802
+	});
+	end();
+	
+	start("steam3id construction (anon gameserver)");
+	sid = new SteamID("[A:1:46124:11245]");
+	checkProperties(sid, {
+		"universe": SteamID.Universe.PUBLIC,
+		"type": SteamID.Type.ANON_GAMESERVER,
+		"instance": 11245,
+		"accountid": 46124
+	});
+	end();
+	
+	start("steamid64 construction");
+	sid = new SteamID("103582791434202956");
+	checkProperties(sid, {
+		"universe": SteamID.Universe.PUBLIC,
+		"type": SteamID.Type.CLAN,
+		"instance": SteamID.Instance.ALL,
+		"accountid": 4681548
+	});
+	end();
+	
+	start("invalid construction");
+	assert.throws(function() {
+		new SteamID("invalid input");
+	}, Error, "expected invalid input to throw Error");
+	end();
+	
+	start("steam2id rendering (universe 0)");
+	sid = new SteamID();
+	sid.universe = SteamID.Universe.PUBLIC;
+	sid.type = SteamID.Type.INDIVIDUAL;
+	sid.instance = SteamID.Instance.DESKTOP;
+	sid.accountid = 46143802;
+	val = sid.getSteam2RenderedID();
+	assert.strictEqual(val, "STEAM_0:0:23071901", "unexpected rendered steam2id value " + val);
+	end();
+	
+	start("steam2id rendering (universe 1)");
+	sid = new SteamID();
+	sid.universe = SteamID.Universe.PUBLIC;
+	sid.type = SteamID.Type.INDIVIDUAL;
+	sid.instance = SteamID.Instance.DESKTOP;
+	sid.accountid = 46143802;
+	val = sid.getSteam2RenderedID(true);
+	assert.strictEqual(val, "STEAM_1:0:23071901", "unexpected rendered steam2id value " + val);
+	end();
+	
+	start("steam2id rendering (non-individual)");
+	sid = new SteamID();
+	sid.universe = SteamID.Universe.PUBLIC;
+	sid.type = SteamID.Type.CLAN;
+	sid.instance = SteamID.Instance.DESKTOP;
+	sid.accountid = 4681548;
+	assert.throws(sid.getSteam2RenderedID.bind(sid), Error, "expected error for rendered steam2id for non-individual type");
+	end();
+	
+	start("steam3id rendering (individual)");
+	sid = new SteamID();
+	sid.universe = SteamID.Universe.PUBLIC;
+	sid.type = SteamID.Type.INDIVIDUAL;
+	sid.instance = SteamID.Instance.DESKTOP;
+	sid.accountid = 46143802;
+	val = sid.getSteam3RenderedID();
+	assert.strictEqual(val, "[U:1:46143802]", "unexpected rendered steam3id value " + val);
+	end();
+	
+	start("steam3id rendering (anon gameserver)");
+	sid = new SteamID();
+	sid.universe = SteamID.Universe.PUBLIC;
+	sid.type = SteamID.Type.ANON_GAMESERVER;
+	sid.instance = 41511;
+	sid.accountid = 43253156;
+	val = sid.getSteam3RenderedID();
+	assert.strictEqual(val, "[A:1:43253156]", "unexpected rendered steam3id value " + val); // TODO: Figure out when to render instanceids
+	end();
+	
+	start("steamid64 rendering (individual)");
+	sid = new SteamID();
+	sid.universe = SteamID.Universe.PUBLIC;
+	sid.type = SteamID.Type.INDIVIDUAL;
+	sid.instance = SteamID.Instance.DESKTOP;
+	sid.accountid = 46143802;
+	val = sid.getSteamID64();
+	assert.strictEqual(val, "76561198006409530", "unexpected rendered steamid64 value " + val);
+	end();
+	
+	start("steamid64 rendering (anon gameserver)");
+	sid = new SteamID();
+	sid.universe = SteamID.Universe.PUBLIC;
+	sid.type = SteamID.Type.ANON_GAMESERVER;
+	sid.instance = 188991;
+	sid.accountid = 42135013;
+	val = sid.getSteamID64();
+	assert.strictEqual(val, "90883702753783269", "unexpected rendered steamid64 value " + val);
+	end();
+	
+	start("invalid id");
+	sid = new SteamID();
+	assert.equal(sid.isValid(), false, "expected new id to be invalid");
+	end();
+} catch(e) {
+	console.log("NOT OK!");
+	console.log(" - " + e.message);
+	process.exit(1); // Throw error
+}
+
+console.log("all tests passed!");
+process.exit(0);
